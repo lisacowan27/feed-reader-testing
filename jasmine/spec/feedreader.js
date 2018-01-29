3189290814a4be62616a118e6b4879ee7eb4acca
lisacowan27 <cowan_lisa@att.net>
2018-01-29 13:36:25 -0600

    feat: add menu suite and test for menu hidden by default

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -51,12 +51,12 @@ $(function() {
                 expect(feed.name.length).not.toBe(0);
             });
         });
-
-
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+
+    describe('Menu functionality', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -64,11 +64,17 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
+        it('menu element hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
