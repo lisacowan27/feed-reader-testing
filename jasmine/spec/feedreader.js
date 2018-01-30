d1a5cfa6da93849f2a9b77d2961a02efafaeb16d
lisacowan27 <cowan_lisa@att.net>
2018-01-30 15:56:59 -0600

    feat: prettify file

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,21 +1,18 @@
 /* $() function to ensure that tests do not run until the DOM is ready */
-
-$(function() {
+$(function () {
 
     /* Global variables */
 
-    var describe,
-    it,
-    expect;
+    var describe, it, expect, allFeeds, beforeEach, loadFeed;
 
     /* Test suite for the RSS feeds definitions */
 
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', function () {
 
         /* Ensure that the allFeed variable is defined and is
         not empty */
 
-        it('allFeeds are defined', function() {
+        it('allFeeds are defined', function () {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -24,19 +21,19 @@ $(function() {
         /* Loops through each feed in the allFeeds object and
         ensure it has a URL defined, and that the URL is not empty. */
 
-        it('allFeeds URL is defined', function() {
-            allFeeds.forEach(function(feed) {
+        it('allFeeds URL is defined', function () {
+            allFeeds.forEach(function (feed) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).not.toBe(0);
             });
-         });
+        });
 
 
         /* Loops through each feed in the allFeeds object and
         ensures it has a name defined, and that the name is not empty. */
 
-        it('allFeeds name is defined', function() {
-            allFeeds.forEach(function(feed) {
+        it('allFeeds name is defined', function () {
+            allFeeds.forEach(function (feed) {
                 expect(feed.name).toBeDefined();
                 expect(feed.name.length).not.toBe(0);
             });
@@ -46,20 +43,20 @@ $(function() {
 
     /* Test suite for the slide out menu */
 
-    describe('The menu', function() {
+    describe('The menu', function () {
 
 
         /* Ensures the menu element is hidden by default. */
 
-        it('menu element hidden by default', function() {
+        it('menu element hidden by default', function () {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
 
-         /* Ensures the menu changes visibility when the menu icon
-         is clicked (toggled). */
+        /* Ensures the menu changes visibility when the menu icon
+        is clicked (toggled). */
 
-        it('menu element transitions when clicked', function() {
+        it('menu element transitions when clicked', function () {
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').click();
@@ -70,49 +67,51 @@ $(function() {
 
     /* Test suite for the Initial Entries */
 
-    describe('Initial Entries', function() {
+    describe('Initial Entries', function () {
 
 
         /* Ensures when the asynchronous loadFeed function is called and completes
         its work, and there is at least a single .entry element within
         the .feed container. */
 
-        beforeEach(function(done) {
-            setTimeout(function() {
+        beforeEach(function (done) {
+            setTimeout(function () {
                 loadFeed(0);
                 done();
             }, 2000);
         });
 
 
-        it('at least 1 single .entry element in .feed container', function(done) {
-            expect($('.feed').children().length).not.toBe(0);
-            done();
-        });
+        it('at least 1 single .entry element in .feed container',
+            function (done) {
+                expect($('.feed').children().length).not.toBe(0);
+                done();
+            });
 
     });
 
     /* Test suite for the New Feed Selection */
 
-    describe('New Feed Selection', function() {
-    var feedContent;
+    describe('New Feed Selection', function () {
+        var feedContent;
 
 
         /* Ensures when a new feed is loaded by the asynchronous loadFeed
         function that the content changes. */
 
-        beforeEach(function(done) {
-            loadFeed(1, function() {
+        beforeEach(function (done) {
+            loadFeed(1, function () {
                 feedContent = $('.feed').html();
-                setTimeout(function() {
+                setTimeout(function () {
                     done();
                 }, 2000);
             });
         });
 
 
-        it('ensures a new feed call loads changed content', function(done) {
-            loadFeed(2, function() {
+        it('ensures a new feed call loads changed content', function (
+            done) {
+            loadFeed(2, function () {
                 expect(feedContent).not.toEqual($('.feed').html());
                 done();
             });
